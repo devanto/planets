c805b07c9dc7ef132f849c9b0aee98ac3f681539
anto <antokantony@yahoo.com>
2020-06-08 18:53:50 +0530

    Updates

---
+++ precache-manifest.b11c69a49245519e2c153897154d1ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db8d7bd4a56d7a3045c279974751dba",
+    "url": "/devanto/planets/index.html"
+  },
+  {
+    "revision": "de793ac087fdb553cca9",
+    "url": "/devanto/planets/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "de793ac087fdb553cca9",
+    "url": "/devanto/planets/static/js/2.0a714cf0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/devanto/planets/static/js/2.0a714cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed10108f95859cc601d",
+    "url": "/devanto/planets/static/js/main.155f301b.chunk.js"
+  },
+  {
+    "revision": "de70e0b9d0b3ca6a851c",
+    "url": "/devanto/planets/static/js/runtime-main.c61d29fc.js"
+  }
+]);
